f683065c76e3040e79e38cc2053bd5c7a2b33464
ARCHIBALD.CARRION <archibald.carrion@ucr.ac.cr>
2022-06-21 21:12:37 -0600

    added content of Ambiante.h

---
+++ Polymorphic_genetic_algorithm/Ambiante.h
@@ -0,0 +1,11 @@
+#ifndef AMBIENTE
+#define AMBIENTE
+#include "Criatura.h"
+
+class Ambiente : public Producto {
+	public:
+	  virtual Criatura * getCriatura()=0;
+	  virtual double fitness(Criatura *)=0;
+	  virtual ~Ambiente(){};
+};
+#endif
