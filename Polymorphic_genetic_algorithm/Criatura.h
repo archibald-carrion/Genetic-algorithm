ed6ef17fef9a28f119ac9399e0fb40c1c1c6d85a
ARCHIBALD.CARRION <archibald.carrion@ucr.ac.cr>
2022-06-21 21:13:23 -0600

    comment main method of creature

---
+++ Polymorphic_genetic_algorithm/Criatura.h
@@ -0,0 +1,7 @@
+/*
+Criatura * mutar(); // Crea una nueva criatuar con una mutación
+Criatura * cruzar( Criatura * ); // Cruza y genera vector de criaturas hijas terminado en 0
+Criatura * clonar();
+Criatura * opuesto(); // Una criatura contraria a la que se tiene
+
+*/
