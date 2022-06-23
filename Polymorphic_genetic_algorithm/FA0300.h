c24e43bbb6a35a1bf6d9dc905ce57fc7f4604118
ARCHIBALD.CARRION <archibald.carrion@ucr.ac.cr>
2022-06-22 22:08:08 -0600

    added the inhereted class from fabrica

---
+++ Polymorphic_genetic_algorithm/FA0300.h
@@ -0,0 +1,11 @@
+#ifndef FA0300_
+#define FA0300_
+#inclide "Fabrica.h"
+
+class FA0300 : private Fabrica{
+	
+	
+	
+};
+
+#endif
---
+++ Polymorphic_genetic_algorithm/FC0300.h
@@ -0,0 +1,11 @@
+#ifndef FC0300_
+#define FC0300_
+#inclide "Fabrica.h"
+
+class FC0300 : private Fabrica{
+	
+	
+	
+};
+
+#endif
