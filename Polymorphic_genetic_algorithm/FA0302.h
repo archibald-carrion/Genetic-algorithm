57b19b245ea558738720c7238e19f780688d2676
ARCHIBALD.CARRION <archibald.carrion@ucr.ac.cr>
2022-06-26 18:18:43 -0600

    added headers content

---
+++ Polymorphic_genetic_algorithm/FA0302.h
@@ -0,0 +1,11 @@
+#ifndef FA0302
+#define FA0302
+#include "Fabrica.h"
+#include "AlgoritmoGenetico.h"
+#include "Producto.h"
+
+class FA0302 : public Fabrica
+{
+    Producto *getProducto();
+};
+#endif
---
+++ Polymorphic_genetic_algorithm/FC0302.h
@@ -0,0 +1,10 @@
+#ifndef FC0302
+#define FC0302
+#include "Fabrica.h"
+#include "Ambiente.h"
+
+class FC0302 : public Fabrica
+{
+    Producto *getProducto();
+};
+#endif
