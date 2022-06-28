1bc9fb07e9ce55597f307cb2fa7abeab69118335
ARCHIBALD.CARRION <archibald.carrion@ucr.ac.cr>
2022-06-27 20:52:51 -0600

    added constant of the class

---
+++ Polymorphic_genetic_algorithm/Bigfoot.h
@@ -0,0 +1,18 @@
+#ifndef BIGFOOT_CRIATURA
+#define BIGFOOT_CRIATURA
+#include "Criatura.h"
+
+#define MAX_ATRIBUTO 99
+#define MIN_ATRIBUTO 0
+#define ALTURA 0
+#define PESO 1
+#define PELAJE 2
+#define VELOCIDAD 3
+#define FUERZA 4
+#define INTELLIGENCIA 5
+
+class Bigfoot : public Criatura
+{
+
+};
+#endif
