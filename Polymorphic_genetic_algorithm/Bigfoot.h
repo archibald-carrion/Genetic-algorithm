cf3b905ed4c5f55304f490f690db0df9a8a920e9
ARCHIBALD.CARRION <archibald.carrion@ucr.ac.cr>
2022-06-27 20:53:10 -0600

    added method and atributes

--- Polymorphic_genetic_algorithm/Bigfoot.h
+++ Polymorphic_genetic_algorithm/Bigfoot.h
@@ -13,6 +13,17 @@
 
 class Bigfoot : public Criatura
 {
+private:
+    int atributo[6];                       // attributos del Bigfoot, varian de 0 a 99
 
+public:
+    Bigfoot();                             // crea un Bigfoot random
+    Bigfoot(int, int, int, int, int, int); // crea un Bigfoot con attributos especificos
+    Criatura *mutar();                     // devuelve la criatura base con mutaciones
+    Criatura **cruzar(Criatura *);         // devuelve un vector de criaturas que termian en 0
+    Criatura *clonar();                    // devuelve una instencia igual a la misma criatura
+    Criatura *opuesto();                   // devuelve el contrario de la criatura
+    int esIgualA(Criatura *);              // verifica si 2 criaturas son iguales
+    ostream &imprimir(ostream &);          // imprime la criatura en la salida estandar
 };
 #endif
