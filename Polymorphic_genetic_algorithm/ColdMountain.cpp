84f67d61f84dc2b26ee6ddf661d633f84935aa4a
ARCHIBALD.CARRION <archibald.carrion@ucr.ac.cr>
2022-06-28 21:03:14 -0600

    added the main skeleton of ColdMountain class

---
+++ Polymorphic_genetic_algorithm/ColdMountain.cpp
@@ -0,0 +1,16 @@
+#include "ColdMountain.h"
+#include "Bigfoot.h"
+
+ColdMountain::ColdMountain()
+{
+}
+
+Criatura *ColdMountain::getCriatura()
+{
+  
+}
+
+double ColdMountain::fitness(Criatura *criatura)
+{
+    // permite saber que tan adaptado criatura es
+}
